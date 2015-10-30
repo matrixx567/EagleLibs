3ac11eb9b69a87cce72766363005c56aa078fbf8
Martin <martin@mnprojects.net>
2015-10-30 22:36:40 +0100

    Remove flatten option in imagemagick

--- sh/print_docu.sh
+++ sh/print_docu.sh
@@ -12,17 +12,17 @@ imageMagickConvert="/c/ImageMagick-6.9.2/convert.exe"
 echo "Convert PDF to PNG"
 
 echo "Convert Schematic"
-$imageMagickConvert -density 400 "$1-schematic.pdf" -quality 95 -flatten -trim  -bordercolor White  -border 2x2 "$1-schematic.png"
+$imageMagickConvert -density 400 "$1-schematic.pdf" -quality 95 -background white -trim  -bordercolor White  -border 2x2 "$1-schematic.png"
 
 echo "Convert Top Assembly"
-$imageMagickConvert -density 400 "$1-assembly-top.pdf" -quality 95 -flatten -trim  -bordercolor White -border 2x2 "$1-assembly-top.png"
+$imageMagickConvert -density 400 "$1-assembly-top.pdf" -quality 95 -background white -trim  -bordercolor White -border 2x2 "$1-assembly-top.png"
 echo "Convert Bottom Assembly"
-$imageMagickConvert -density 400 "$1-assembly-bottom.pdf" -quality 95 -flatten -trim  -bordercolor White -border 2x2 "$1-assembly-bottom.png"
+$imageMagickConvert -density 400 "$1-assembly-bottom.pdf" -quality 95 -background white -trim  -bordercolor White -border 2x2 "$1-assembly-bottom.png"
 
 echo "Convert Top Copper"
-$imageMagickConvert -density 400 "$1-copper-top.pdf" -quality 95 -flatten -trim  -bordercolor White -border 2x2 "$1-copper-top.png"
+$imageMagickConvert -density 400 "$1-copper-top.pdf" -quality 95 -background white -trim  -bordercolor White -border 2x2 "$1-copper-top.png"
 echo "Convert Bottom Copper"
-$imageMagickConvert -density 400 "$1-copper-bottom.pdf" -quality 95 -flatten -trim  -bordercolor White -border 2x2 "$1-copper-bottom.png"
+$imageMagickConvert -density 400 "$1-copper-bottom.pdf" -quality 95 -background white -trim  -bordercolor White -border 2x2 "$1-copper-bottom.png"
 
 rm $1-schematic.pdf $1-assembly-top.pdf $1-assembly-bottom.pdf $1-copper-top.pdf $1-copper-bottom.pdf
 
